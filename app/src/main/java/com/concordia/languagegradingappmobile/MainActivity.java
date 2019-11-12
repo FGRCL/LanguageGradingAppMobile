1f7971a206d3170db957d39766c1eac88e8cf715
François <Fgrclaberge@gmail.com>
2019-11-12 02:14:23 -0500

    Add Volley to dependencies

--- app/src/main/java/com/concordia/languagegradingappmobile/MainActivity.java
+++ app/src/main/java/com/concordia/languagegradingappmobile/MainActivity.java
@@ -1,14 +1,60 @@
 package com.concordia.languagegradingappmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button sendButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sendButton = (Button) findViewById(R.id.sendButton);
+        sendButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                volleyDemo();
+            }
+        });
+    }
+
+    private void volleyDemo(){
+        final TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText("Hiya");
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://www.google.com";
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) == PackageManager.PERMISSION_GRANTED) {
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            // Display the first 500 characters of the response string.
+                            textView.setText("Response is: "+ response.substring(0,500));
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    textView.setText("That didn't work!");
+                }
+            });
+            queue.add(stringRequest);
+        } else {
+            textView.setText("You don't have permission");
+        }
+
     }
 }
